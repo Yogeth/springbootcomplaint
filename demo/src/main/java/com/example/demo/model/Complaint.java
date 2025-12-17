00c96cddd248b4e0f4bde97bb1306f254bacbf25
Yogeth <yogethcr@gmail.com>
2025-12-18 04:50:22 +0530

    update Complaint.java

--- demo/src/main/java/com/example/demo/model/Complaint.java
+++ demo/src/main/java/com/example/demo/model/Complaint.java
@@ -7,9 +7,9 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Complaint{
- private name;
+ private String name;
  @Id
- private email;
- private issues;
- private message;
+ private String email;
+ private  String issues;
+ private  String message;
 }
