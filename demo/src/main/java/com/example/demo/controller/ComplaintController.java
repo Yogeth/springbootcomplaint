f96ef2f0b80ba353f2c36bc0027f2f656b7a6b47
Yogeth <yogethcr@gmail.com>
2025-12-18 18:07:50 +0530

    update ComplaintController.java

--- demo/src/main/java/com/example/demo/controller/ComplaintController.java
+++ demo/src/main/java/com/example/demo/controller/ComplaintController.java
@@ -6,7 +6,7 @@ import com.example.demo.service.ComplaintService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 @RestController
-@CrossOrigin(orgins="http://localhost:8080")
+@CrossOrigin(origins="http://localhost:8080")
 public class ComplaintController{
   
   @Autowired
